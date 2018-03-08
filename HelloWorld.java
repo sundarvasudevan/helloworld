94990ba8532df1e206397632b80c41499b9bd32a
Sundar Vasudevan <sundar.vasudevan.sv@gmail.com>
2018-03-08 16:35:31 +0530

    This is the constructor version of HelloWorld Class.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,27 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    private int x;
+    private int y;
+    public HelloWorld()
+    {
+        x=10; y=20;
+    }
+
+    public int addXY(int x, int y)
+    {
+        this.x =x;
+        this.y = y;
+        return (x+y);
+    }
+
+    public static void main(String[] args)
+    {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+         HelloWorld h  = new HelloWorld();
+         int xy = h.addXY(2,8);
+         System.out.println(xy);
+
     }
 
 }
